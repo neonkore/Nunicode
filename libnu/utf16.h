9bb8f3e59b946342ab2bfadcbe28199970259fe5
Aleksey Tulinov <aleksey.tulinov@gmail.com>
2013-10-20 19:19:51 +0300

    +accumulated bom detection

--- libnu/utf16.h
+++ libnu/utf16.h
@@ -20,22 +20,24 @@ typedef char* (*nu_utf16_write_bom_t)(char *);
 
 #ifdef NU_WITH_UTF16_READER
 
+typedef struct {
+	nu_read_iterator_t read;
+	nu_write_iterator_t write;
+	nu_revread_iterator_t revread;
+	nu_utf16_write_bom_t write_bom;
+} nu_utf16_bom_t;
+
 /** Read BOM from encoded string
  *
  * @ingroup utf16
  * @param encoded pointer to encoded strings
- * @param bom optional, return pointer to detected bom write function.
- * Note it will always be 0 if NU_WITH_UTF16_WRITER is not enabled
- * @param rd optional, return pointer to detected decoding function
- * @param wr optional, return pointer to detected encoding function
- * @param revrd optional, return pointer to detected reverse read (decode) function.
- * Note it will always be 0 if NU_WITH_REVERSE_READ is not enabled
+ * @param bom optional, this struct will be filled with read, write, etc
+ * function for detected BOM. Note revread and write might be 0 if not
+ * enabled in build options
  * @return pointer to next character in UTF-16 string or 0 if BOM is
  * not found
  */
-const char* nu_utf16_read_bom(const char *encoded, nu_utf16_write_bom_t *bom, 
-	nu_read_iterator_t *rd, nu_write_iterator_t *wr,
-	nu_revread_iterator_t *revrd);
+const char* nu_utf16_read_bom(const char *encoded, nu_utf16_bom_t *bom);
 
 #endif /* NU_WITH_UTF16_READER */
 
