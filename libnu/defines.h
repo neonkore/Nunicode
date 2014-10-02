44367e51d7211658bc9d5d497df4c0e6dcb89356
Aleksey Tulinov <aleksey.tulinov@gmail.com>
2014-10-03 00:06:58 +0300

    +visibility hidden for static build +library size optimization options

--- libnu/defines.h
+++ libnu/defines.h
@@ -2,11 +2,21 @@
 #define NU_DEFINES_H
 
 #ifndef NU_EXPORT
+
 # ifdef _WIN32
 #  define NU_EXPORT __declspec(dllexport)
+
+# elif __GNUC__ >= 4
+#  ifdef BUILD_STATIC
+#   define NU_EXPORT __attribute__ ((visibility ("hidden")))
+#  else
+#   define NU_EXPORT __attribute__ ((visibility ("default")))
+#  endif
+
 # else
 #  define NU_EXPORT
 # endif
+
 #endif /* NU_EXPORT */
 
 #define NU_UNICODE_VERSION 700
