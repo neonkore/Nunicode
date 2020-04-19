6eacaaf4a3d7660f33a5a06f30a7ff35d8d998a2
Алексей Тулинов <aleksey.tulinov@gmail.com>
2020-04-19 22:10:16 +0300

    +(wip) mph for contractions roots

--- tools/contractions-toc.go
+++ tools/contractions-toc.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -35,15 +36,14 @@ func main() {
 	}
 
 	files := os.Args[1:3] // codepoints file, contractions file
-	tag := os.Args[3]
-	// tag, bmpOnly := os.Args[3], false
+	tag, compact := os.Args[3], false
 
-	// bmpOnlyOpt, err := strconv.ParseBool(os.Args[4])
-	// if err != nil {
-	// 	fmt.Fprintln(os.Stderr, err)
-	// 	os.Exit(1)
-	// }
-	// bmpOnly = bmpOnlyOpt
+	compactOpt, err := strconv.ParseBool(os.Args[4])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	compact = compactOpt
 
 	codepoints, contractions, err := collectContractions(files)
 	if err != nil {
@@ -58,6 +58,18 @@ func main() {
 	}
 
 	roots := getRootStates(states)
+	rootsMapping := MPHMapping{}
+
+	for _, state := range roots {
+		var codepoint uint32 = state.codepoints[0]
+		rootsMapping[codepoint] = MPHV{
+			codepoint:   codepoint,
+			replacement: uint16(state.flatWeight), // FIXME: narrowing cast uint32 -> uint16
+		}
+	}
+
+	rootsG, rootsV := createMPH(rootsMapping)
+	rootsC, rootsI := splitCodepointsAndIndices(rootsV)
 
 	contractionsRoots := make([]ContractionsRootType, 0)
 	for _, state := range roots {
@@ -74,6 +86,8 @@ func main() {
 		}
 	}
 
+	TAG, rootsTAG := strings.ToUpper(tag), strings.ToUpper(tag+"_roots")
+
 	check(genContractionsHeader(sink, ContractionsHeaderTags{
 		Tool:         path.Base(os.Args[0]),
 		Unixtime:     time.Now().Unix(),
@@ -82,12 +96,28 @@ func main() {
 	}))
 	check(genContractionsIncludes(sink, ContractionsIncludesTags{}))
 	check(genContractionsConsts(sink, ContractionsConstsTags{
-		TAG:          strings.ToUpper(tag),
+		TAG:          TAG,
 		Codepoints:   uint(len(codepoints)),
 		Contractions: uint(len(contractions)),
 	}))
 	check(genContractionsRoots(sink, ContractionsRootsTags{
 		TAG:   strings.ToUpper(tag),
 		Roots: contractionsRoots,
+	}))
+	check(genMPHG(sink, MPHGTags{
+		Tag:       rootsTAG,
+		G:         rootsG,
+		Linebreak: makeLinebreakFunc(10),
+	}))
+	check(genMPHC(sink, MPHCTags{
+		Compact:   compact,
+		Tag:       rootsTAG,
+		C:         rootsC,
+		Linebreak: makeLinebreakFunc(8),
+	}))
+	check(genMPHI(sink, MPHITags{
+		Tag:       rootsTAG,
+		I:         rootsI,
+		Linebreak: makeLinebreakFunc(10),
 	}))
 }
